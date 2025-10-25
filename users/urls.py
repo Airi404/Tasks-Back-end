e9b7908feae6173749bc0ec8b04c55542abfdfce
Iria <airicastega@gmail.com>
2025-10-25 12:32:43 +0200

    commit 2

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import register_view
+from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', register_view, name='register')
+    path('register/', register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_user, name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('home')  # O cambia 'home' por la vista que desees
+            user = form.save()
+            login(request, user)
+            return redirect('home')
     else:
-        form = UserCreationForm()
+        form = UserCreationForm() 
+
     return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'users/login.html', {'form': form})
+
+def logout_user(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
